37b96747dbcf786507b5250d1294530af1c0d9bd
Sadia Romain <sadiaromain@gmail.com>
2021-02-08 11:30:23 -0500

    update package vulnerabilities in archi

---
+++ projects/archi/precache-manifest.918884115c12d754af1ca688449b345e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b003bfacd088a2f9fb234889edaf8f",
+    "url": "/projects/archi/index.html"
+  },
+  {
+    "revision": "4231fcc4610c6f1dd4bf",
+    "url": "/projects/archi/static/css/main.b771c5e9.chunk.css"
+  },
+  {
+    "revision": "cbe301f320821afe5a89",
+    "url": "/projects/archi/static/js/2.ae11f141.chunk.js"
+  },
+  {
+    "revision": "390b43b777af03f03ef4ce5c088d6f2a",
+    "url": "/projects/archi/static/js/2.ae11f141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4231fcc4610c6f1dd4bf",
+    "url": "/projects/archi/static/js/main.788b4c40.chunk.js"
+  },
+  {
+    "revision": "51fd6cbc79e02ea6eefb",
+    "url": "/projects/archi/static/js/runtime-main.526856fa.js"
+  }
+]);
