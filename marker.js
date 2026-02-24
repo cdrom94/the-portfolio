40dbef5eac7e7a6dac6b4ff3810b7f953091609b
Sadia <sadiaromain@gmail.com>
2026-02-24 00:57:06 -0500

    adjust marker weight to 0.4 and increase frame rate

--- marker.js
+++ marker.js
@@ -3,22 +3,22 @@ let trail = [];
 function setup() {
 	createCanvas(windowWidth, windowHeight, WEBGL);
 	angleMode(DEGREES);
+
+	brush.set("marker", "#4ad6af", 0.4);
 }
 
 function draw() {
 	background(255, 1);
-	frameRate(30);
+	frameRate(60);
 	translate(-width / 2, -height / 2);
 
 	if (mouseX !== 0 || mouseY !== 0) {
 		trail.push([pmouseX, pmouseY]);
 	}
 
-	if (trail.length > 15) {
+	if (trail.length > 40) {
 		trail.shift();
 	}
-
-	brush.set("marker", "#4ad6af", 0.25);
 
 	brush.spline(trail);
 }
